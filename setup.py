b6ac3e7756c39b5a67fa9129ba1daf3baaeeade0
Charith Atapattu <31107032+charithsiu@users.noreply.github.com>
2020-11-20 01:06:42 -0600

    Initial Commit
    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyoctonion",
+    version="3.3",
+    description="Octonion Number System",
+    packages=["pyoctonion"],
+    author="Kalpa Madawa & Charith Atapattu",
+    author_email="charithnisanka@gmail.com",
+    zip_safe=False,
+)
